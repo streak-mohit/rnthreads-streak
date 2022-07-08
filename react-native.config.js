10a4ab0eb0a5037b7e71087fd88fd6ec7737bb03
Mohit Kumar <80596198+streak-mohit@users.noreply.github.com>
2022-07-08 15:28:55 +0530

    Update react-native.config.js

--- react-native.config.js
+++ react-native.config.js
@@ -2,8 +2,9 @@ module.exports = {
   dependency: {
     platforms: {
       android: {
+        packageImportPath: 'import com.reactlibrary.RNThreadPackage;',
         packageInstance: 'new RNThreadPackage(reactNativeHost)',
-      },
+      }, 
     },
   },
-};
+}
